dcf45055887060108cd3a4442b3aec71d655478c
aghastmuffin <78246775+aghastmuffin@users.noreply.github.com>
2024-05-06 19:21:24 -0400

    loooooking FINEEE

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ async function check(){
     document.getElementById("load").innerText = "Loading...";
     let pyodide = await loadPyodide();
     let a = document.getElementById("cin").value;
+    if (a.length == 0) {
+        document.getElementById("cout").innerText = "Please enter some code.";
+        document.getElementById("load").innerText = "";
+        return;
+    }
     let pythonCode = `
 def run_and_capture():
     import io
